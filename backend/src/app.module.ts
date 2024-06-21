83b417bb40002587ed920d8bbbd2b16aa4c6a72e
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-21 17:28:08 -0300

    chore: serverless config corretion

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { TestController } from './test.controller';
 
 @Module({
   imports: [],
-  controllers: [TestController],
 })
 export class AppModule {}

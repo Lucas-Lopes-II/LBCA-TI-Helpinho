0cdad157d7b74dedea5702343b6f9086b64f215b
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-21 11:26:07 -0300

    feat: StrongPasswordValidation - implemantation

--- backend/src/@shared/domain/validations/index.ts
+++ backend/src/@shared/domain/validations/index.ts
@@ -2,4 +2,5 @@ export * from './validator';
 export * from './uuid.validation';
 export * from './validation.inteface';
 export * from './required-field.validation';
+export * from './strong-password.validation';
 // export * from './';

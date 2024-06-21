4954e8060683333dfcfea1c84555e0365323d74d
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-21 11:24:49 -0300

    feat: UUIDValidation - implemantation

--- backend/src/@shared/domain/validations/index.ts
+++ backend/src/@shared/domain/validations/index.ts
@@ -1,4 +1,5 @@
 export * from './validator';
+export * from './uuid.validation';
 export * from './validation.inteface';
 export * from './required-field.validation';
-// export * from './'
+// export * from './';

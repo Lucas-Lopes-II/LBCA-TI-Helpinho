b03711711f16a9b2257f58c7461adb783be5883e
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-21 13:49:45 -0300

    chore: run serverless service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { TestController } from './test.controller';
+
 @Module({
   imports: [],
+  controllers: [TestController],
 })
 export class AppModule {}

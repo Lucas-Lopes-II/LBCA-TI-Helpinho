be469ff1cd05b7136f0ce74fd72f120618d6d2ca
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-06-22 13:50:55 -0300

    feat: IHelpsRepository creation

---
+++ backend/src/modules/helps/data/helps.repository.interface.ts
@@ -0,0 +1,10 @@
+import { Help } from '@helps/data';
+import { ICreate, IDelete, IFindById, IUpdate } from '@shared/infra/data';
+
+export interface IHelpsRepository
+  extends ICreate<Help, void>,
+    IFindById<Help>,
+    IDelete,
+    IUpdate<Help> {
+  findByEmail(email: string): Promise<Help | undefined>;
+}
---
+++ backend/src/modules/helps/data/index.ts
@@ -0,0 +1,4 @@
+export * from './help';
+export * from './help-category.enum';
+export * from './helps.repository.interface';
+// export * from './';

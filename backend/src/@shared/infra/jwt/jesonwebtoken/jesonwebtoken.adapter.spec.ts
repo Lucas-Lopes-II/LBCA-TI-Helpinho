b1dbb63efe64b61210834b22fd0b1de5f05fcaab
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-06-22 20:13:22 -0300

    test: JsonWebTokenAdapter -  unit tests

---
+++ backend/src/@shared/infra/jwt/jesonwebtoken/jesonwebtoken.adapter.spec.ts
@@ -0,0 +1,46 @@
+import { UnauthorizedError } from '@shared/domain/errors';
+import { JsonWebTokenAdapter } from './jesonwebtoken.adapter';
+import { JwtFactory } from '@shared/infra/jwt';
+
+describe('JsonWebTokenAdapter integration tests', () => {
+  const secretKey = 'mySecretKey';
+  const sut = JwtFactory.createWithGivenSecretKey(secretKey);
+
+  it('should create and decode a JWT token correctly', () => {
+    const payload = { userId: 12345 };
+    const token = sut.sign(payload, { expiresIn: '1h' });
+    const decodedPayload = sut.decode(token);
+
+    expect(decodedPayload['userId']).toEqual(12345);
+    expect(decodedPayload['exp']).toBeDefined();
+    expect(decodedPayload['iat']).toBeDefined();
+  });
+
+  it('should throw an UnauthorizedError if the JWT token is expired', async () => {
+    const payload = { userId: 12345 };
+
+    const token = sut.sign(payload, { expiresIn: '1s' });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    expect(() => sut.verify<{ userId: number }>(token)).toThrow(
+      new UnauthorizedError('Token JWT expired'),
+    );
+  });
+
+  it('should throw an UnauthorizedError if the JWT token is not valid', async () => {
+    expect(() => sut.verify('invalid token')).toThrow(
+      new UnauthorizedError('Action not allowed'),
+    );
+  });
+
+  it('should not accept JWT token in valid format but with another secret key', async () => {
+    const anotherSecretKey = 'anotherSecretKey';
+    const another = new JsonWebTokenAdapter(anotherSecretKey);
+    const payload = { userId: 12345 };
+    const token = another.sign(payload, { expiresIn: '1h' });
+
+    expect(() => sut.verify<{ userId: number }>(token)).toThrow(
+      new UnauthorizedError('Action not allowed'),
+    );
+  });
+});

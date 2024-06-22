8b6f99d340eaa45e8ad5ece158ece2308c5a69f7
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-06-22 20:37:16 -0300

    feat: AuthController - login endpoint completion

--- backend/src/modules/auth/main/auth.controller.ts
+++ backend/src/modules/auth/main/auth.controller.ts
@@ -3,14 +3,23 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './guards';
+import { IsPublic } from '@shared/infra/decorators';
+import { AuthRequest } from './dtos';
+import { AuthUseCasesFactory } from '@auth/usecases';
 
 @Controller('auth')
 export class AuthController {
-  @Post('login')
-  @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  public async login() {}
+  @HttpCode(HttpStatus.OK)
+  @IsPublic()
+  @Post('login')
+  public async login(@Req() req: AuthRequest) {
+    const usecase = AuthUseCasesFactory.generateSigninToken();
+
+    return usecase.execute(req.user);
+  }
 }

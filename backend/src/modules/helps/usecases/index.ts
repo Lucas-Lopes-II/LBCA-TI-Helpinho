3a831aa37c01abdb037d12f17d14abdb2cfecc54
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-24 14:12:34 -0300

    refactor: change helps usecases folders

--- backend/src/modules/helps/usecases/index.ts
+++ backend/src/modules/helps/usecases/index.ts
@@ -1,6 +1,7 @@
 export * from './helps.usecases.factory';
-export * from './create/create-help.usecase';
-export * from './delete/delete-help.usecase';
-export * from './search/seacrh-helps.usecase';
-export * from './find-by-id/find-by-id.usecase';
+export * from './helps/create/create-help.usecase';
+export * from './helps/delete/delete-help.usecase';
+export * from './helps/search/seacrh-helps.usecase';
+export * from './helps/find-by-id/find-by-id.usecase';
+export * from './helps-provided/create/create-help-provided.usecase';
 // export * from './';

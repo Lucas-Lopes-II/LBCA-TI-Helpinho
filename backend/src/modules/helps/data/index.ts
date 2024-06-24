f88c4722175047a9ca8fee43127a4d4f7b3c0bdf
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-06-24 18:11:19 -0300

    feat: HelpsProvidedRepositoryFactory - implemantation

--- backend/src/modules/helps/data/index.ts
+++ backend/src/modules/helps/data/index.ts
@@ -5,5 +5,6 @@ export * from './repositories/helps.repository';
 export * from './repositories/helps.repository.factory';
 export * from './repositories/helps-provided.repository';
 export * from './repositories/helps.repository.interface';
+export * from './repositories/helps-provided.repository.factory';
 export * from './repositories/helps-provided.repository.interface';
 // export * from './';

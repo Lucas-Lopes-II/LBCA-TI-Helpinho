99ae7f775ae31caae29fce86a9a49080a60ad468
Fulano de Tal <lucas.santos.pessoal@outlook.com>
2024-06-26 13:08:54 -0300

    feat: HelpsRepository - searchByFilter method implemantation

---
+++ backend/src/@shared/infra/data/filter-indexes.dto.ts
@@ -0,0 +1,5 @@
+export type FilterIndexes<I = unknown, F = unknown, T = unknown> = {
+  index: I;
+  field: F;
+  value: T;
+};
--- backend/src/modules/helps/data/repositories/helps/helps.repository.interface.ts
+++ backend/src/modules/helps/data/repositories/helps/helps.repository.interface.ts
@@ -1,15 +1,33 @@
 import {
+  FilterIndexes,
   ICreate,
   IDelete,
   IFindById,
   ISearchableRepository,
   IUpdate,
+  SearchParams,
+  SearchResult,
 } from '@shared/infra/data';
 import { Help } from '../../entities/help';
+
+export enum HelpsIndexes {
+  ID = 'IdIndex',
+  USER_HELPED_ID = 'UserHelpedIndex',
+}
+
+export enum HelpsFields {
+  ID = 'id',
+  USER_HELPED_ID = 'userHelped',
+}
 
 export interface IHelpsRepository
   extends ICreate<Help, void>,
     IFindById<Help>,
     IDelete,
     ISearchableRepository<Help>,
-    IUpdate<Help> {}
+    IUpdate<Help> {
+  searchByFilter(
+    props: SearchParams,
+    filter: FilterIndexes<HelpsIndexes, HelpsFields, string>,
+  ): Promise<SearchResult<Help>>;
+}

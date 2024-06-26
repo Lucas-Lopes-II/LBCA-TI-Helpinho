618cfae9c35ec3639b47b8e303fb83155f78bff4
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2024-06-26 09:41:23 -0300

    feat: NumberFloatPipe - implemantation

--- frontend/src/app/shared/pipes/index.ts
+++ frontend/src/app/shared/pipes/index.ts
@@ -1,3 +1,4 @@
 export * from './formata-data.pipe';
+export * from './number-float.pipe';
 export * from './shared-pipes-module.module';
 // export * from './';
--- frontend/src/app/shared/pipes/shared-pipes-module.module.ts
+++ frontend/src/app/shared/pipes/shared-pipes-module.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormataDataPipe } from './formata-data.pipe';
+import { NumberFloatPipe } from './number-float.pipe';
 
 @NgModule({
-  declarations: [FormataDataPipe],
+  declarations: [FormataDataPipe, NumberFloatPipe],
   imports: [CommonModule],
-  exports: [FormataDataPipe],
+  exports: [FormataDataPipe, NumberFloatPipe],
 })
 export class SharedPipesModuleModule {}
